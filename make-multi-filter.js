f76ef415cd648a62c5ab923590ede388092b4126
TrustyTraitor <gainmikey@gmail.com>
2024-09-03 09:27:42 -0400

    completed problem 2

---
+++ make-multi-filter.js
@@ -0,0 +1,27 @@
+'use strict';
+
+function MakeMultiFilter(originalArray) {
+    let currentArray = [...originalArray];
+
+    function arrayFilterer(filterCriteria, callback) {
+        if (typeof filterCriteria === 'function') {
+            currentArray = currentArray.filter(filterCriteria);
+            
+            if (typeof callback === 'function') {
+				const boundCallback = callback.bind(originalArray);
+                boundCallback(currentArray);
+			}
+        } else {
+            if (typeof callback === 'function') {
+				const boundCallback = callback.bind(originalArray);
+                boundCallback(currentArray);
+            }
+
+			return currentArray;
+        }
+        
+        return arrayFilterer;
+    }
+    
+    return arrayFilterer;
+}
